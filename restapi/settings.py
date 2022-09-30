3eebecfab1aa72e1e36618beadc5113b6edbdce8
Kuro32183 <takumi32183@gmail.com>
2022-10-01 01:03:32 +0900

    add app name

--- restapi/settings.py
+++ restapi/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'restapi'
     'rest_framework',
     'api.apps.ApiConfig',
     'corsheaders',
